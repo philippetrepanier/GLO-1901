d88bf63a01e21a0507dd17a60b8cd8a8a5f3f783
Jérémie Roy <25518631+jereroy@users.noreply.github.com>
2016-11-24 12:31:57 -0500

    Commencement de argparse

--- sudoku.py
+++ sudoku.py
@@ -2,5 +2,16 @@ import affichage
 import solver
 import lecture
 import console
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-m", "--manuel", action="store_true", help="Mode manuel")
+parser.add_argument("-a", "--automatique", action="store_true", help="Mode automatique")
+
+args = parser.parse_args()
+
+if args.manuel:
+    print("Mode Manuel")
+if args.automatique:
+    print("Mode Automatique")
 print("Sudoku Pythonesque")
