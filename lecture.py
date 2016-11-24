369242d1fbf425a3f74a09980f079a81fa609815
phtre60 <phtre60@localhost>
2016-11-24 14:17:50 -0500

    Traitement initial grille

--- lecture.py
+++ lecture.py
@@ -1,9 +1,11 @@
 print("Lecture")
 
-#Lecture du fichier
+##Lecture du fichier
+
+
 class Fichier:
     def __init__(self, fichiergrille):
-        self.lignesfichier =''
+        self.lignesfichier = ''
         for i in fichiergrille:
             self.lignesfichier += ''.join(i).strip()
         self.lignesfichier = self.lignesfichier.replace(' ', '')
@@ -11,11 +13,16 @@ class Fichier:
             assert i.isdigit() == True or i == '.', \
                 'Le fichier grille ne doit contenir que des chiffres, des' \
                 'points, des espaces ou des retour de lignes. Vérifiez l\'entrée'
+        assert len(self.lignesfichier) % 81 == 0, 'La longueur de la grille n\'est pas valide'
     def imprimmer(self):
         print(self.lignesfichier)
+
     def convertirEnGrille(self):
-        print('OK')
+        nombregrille = len(self.lignesfichier) // 81
+        for i in nombregrille:
+
 
 class Grille:
     def __init__(self, lignesfichier):
         self.dictionnaire = 2
+        self.valeurs = ""
