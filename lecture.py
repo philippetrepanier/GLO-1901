c23bebe92ddf643d6f693155852d5111c5631088
Jérémie Roy <25518631+jereroy@users.noreply.github.com>
2016-11-24 13:29:25 -0500

    Lecture du fichier d?vi

--- lecture.py
+++ lecture.py
@@ -1,2 +1,12 @@
 print("Lecture")
 
+#Lecture du fichier
+class Fichier:
+    def __init__(self, fichiergrille):
+        self.lignesfichier =''
+        for i in fichiergrille:
+            self.lignesfichier += ''.join(i).strip()
+        self.lignesfichier = self.lignesfichier.replace(' ', '')
+
+    def lire(self):
+        print(self.lignesfichier)
