3b4aa72f1fbf1a513591adb9a513939577fc2677
jeroy154 <jeremie.roy.5@ulaval.ca>
2016-12-05 14:35:26 -0500

    Console presque fini (Count -> Compte)

--- console.py
+++ console.py
@@ -10,8 +10,8 @@ class Console:
             for c in Grille.colonnes:
                 liste.append(reponse[l][c])
             res += ' ' + l + ' \u2551 {0[0]}  {0[1]}  {0[2]} \u2551 {0[3]}  {0[4]}  {0[5]} \u2551 {0[6]}  {0[7]}  {0[8]} \u2551'.format(liste) + '\n'
-            count += 1
-            if count == 3 or count == 6:
+            compte += 1
+            if compte == 3 or count == 6:
                 res += 3 * ' ' + "\u2560" + "\u2550" * 9 + "\u256C" + "\u2550" * 9 + "\u256C" + "\u2550" * 9 + "\u2563" + "\n"
         res += "   \u255A" + "\u2550" * 9 + "\u2569" + "\u2550" * 9 + "\u2569" + "\u2550" * 9 + "\u255D"
     print(res)
