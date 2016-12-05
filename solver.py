c17790d64bbce51556c1aa9690c1a87284d6bca4
Philippe Trepanier <philippetrepanier@users.noreply.github.com>
2016-12-05 11:58:37 -0500

    Fonction resolu

--- solver.py
+++ solver.py
@@ -8,15 +8,14 @@ class Grille:
         # Définitions de concepts généraux
         self.colonnes = "123456789"
         self.lignes = "ABCDEFGHI"
-        self.nombres = self.colonnes
-        self.cases ={}
+        self.cases = {}
 
         compte = 0
         for l in self.lignes:
             self.cases[l] = {}
             for c in self.colonnes:
                 if lignesfichier[compte] == "." or lignesfichier[compte] == "0":
-                    self.cases[l][c] = self.nombres
+                    self.cases[l][c] = self.colonnes
                 else:
                     self.cases[l][c] = lignesfichier[compte]
                 compte += 1
@@ -30,13 +29,15 @@ class Grille:
                     for l2 in self.lignes:
                         print(l2, c, self.cases[l2][c])
                         if len(self.cases[l2][c]) == 1 and (l, c) != (l2, c):
+                            print(self.cases[l2][c])
+
+
+
                             self.cases[l][c] = self.cases[l][c].replace(str(self.cases[l2][c]), 'pou')
-                            print('POURR')
+                            #print('POURR')
 
                     # for carrés'''
 
-        print(self.cases)
-        print(self.cases['A']['1'])
 
     # Permet de vérifier si toute la grille contient un élément
     def resolu(self):
