2f505f5fa4598f2fa833dd6414916cd2993fd73c
Jérémie Roy <25518631+jereroy@users.noreply.github.com>
2016-12-05 13:34:52 -0500

    Console

--- console.py
+++ console.py
@@ -1,29 +1,18 @@
 print("Console")
 
 class Console:
-    def __init__(self):
-        self.data ="PU"
-    def afficherConsole(self, reponse):
-        #On devrait splitter la liste en 9 liste
-        verticale = ['A','B','C','D','E','F','G','H','I']
-        listeligne = reponse.split(9)
-        res = '   123 456 789'\n'  +---+---+---+'
-        for ligne in listeligne:
-            res += Verticale[ligne] + ' |'
-            for nombre in ligne:
-                res += '|' + ligne[nombre]
-        res += '+---+---+---+'
-
-class Console:
-    def __init__(self,reponse):
-        self.reponse[l][c] #???
     def __str__(self, reponse):
-        res = "     1  2  3   4  5  6   7  8  9 \n   \u2554" + "\u2550" * 9 + "\u2566" + "\u2550" * 9 + "\u2566" + "\u2550" * 9 + "\u2557" + '\n'
+        count = 0
+        res = "     1  2  3   4  5  6   7  8  9 \n   \u2554" + "\u2550" * 9 + "\u2566" + "\u2550" * 9 + "\u2566" + \
+              "\u2550" * 9 + "\u2557" + '\n'
         for l in Grille.lignes:
+            liste = []
             for c in Grille.colonnes:
-                res += ' ' + c + ' \u2551 {}  {}  {} \u256C  {}  {}  {} \u256C {}  {}  {} \u2563'.format(reponse[l][c])
+                liste.append(reponse[l][c])
+            res += ' ' + l + ' \u2551 {}  {}  {} \u256C  {}  {}  {} \u256C {}  {}  {} \u2563'.format(liste)
+            count += 1
+            if count == 3 or 6:
+                res +='   \u2554" + " " * 9 + "\u256C" + " " * 9 + "\u256C" + " " * 9 + "\u2563" + "\n"'
         res += "   \u255A" + "\u2550" * 9 + "\u2569" + "\u2550" * 9 + "\u2569" + "\u2550" * 9 + "\u255D"
-
-
-
+    print(res)
 
